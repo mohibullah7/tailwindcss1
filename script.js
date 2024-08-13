91d477f06e6aa58c8047060ec38e0c760271c388
Mohib khan mahsud <f20bseen1m01107@iub.edu.pk>
2024-08-13 23:12:49 +0500

    thi is my tailwind css project

---
+++ script.js
@@ -0,0 +1,56 @@
+function show(){
+  document.getElementById("para1").style.display="block";
+  document.getElementById("icon1").style.display='none';
+  document.getElementById("icon2").style.display="inline-block";
+}
+
+function closee(){
+  document.getElementById("icon1").style.display="inline-block";
+  document.getElementById("para1").style.display='none';
+  document.getElementById("icon2").style.display='none';
+}
+function show1(){
+  document.getElementById("para2").style.display="block";
+  document.getElementById("icon3").style.display='none';
+  document.getElementById("icon4").style.display="inline-block";
+}
+
+function closee1(){
+  document.getElementById("icon3").style.display="inline-block";
+  document.getElementById("para2").style.display='none';
+  document.getElementById("icon4").style.display='none';
+}
+function show2(){
+  document.getElementById("para3").style.display="block";
+  document.getElementById("icon5").style.display='none';
+  document.getElementById("icon6").style.display="inline-block";
+}
+
+function closee2(){
+  document.getElementById("icon5").style.display="inline-block";
+  document.getElementById("para3").style.display='none';
+  document.getElementById("icon6").style.display='none';
+}
+function show(){
+  document.getElementById("para1").style.display="block";
+  document.getElementById("icon1").style.display='none';
+  document.getElementById("icon2").style.display="inline-block";
+}
+
+function closee(){
+  document.getElementById("icon1").style.display="inline-block";
+  document.getElementById("para1").style.display='none';
+  document.getElementById("icon2").style.display='none';
+}
+function show(){
+  document.getElementById("para1").style.display="block";
+  document.getElementById("icon1").style.display='none';
+  document.getElementById("icon2").style.display="inline-block";
+}
+
+function closee(){
+  document.getElementById("icon1").style.display="inline-block";
+  document.getElementById("para1").style.display='none';
+  document.getElementById("icon2").style.display='none';
+}
+
